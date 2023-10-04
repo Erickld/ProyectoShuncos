67e514c17a71a9f48cfa34d389f218e0ba8b1737
Erickld <erickld.24@gmail.com>
2023-10-04 16:12:55 -0600

    ajustes urls

--- javascripts/loadFooter.js
+++ javascripts/loadFooter.js
@@ -1,4 +1,7 @@
 const containerFooter = document.querySelector('#footer-container');
-fetch('/html/footerTemplate.html').then(res => res.text()).then(data => {
+const fuenteFooter = document.currentScript.getAttribute('fuente');
+const urlFooter = (fuenteFooter == "root") ? ('./html/footerTemplate.html') : ('./footerTemplate.html');
+
+fetch(urlFooter).then(res => res.text()).then(data => {
     containerFooter.innerHTML = data;
 })
