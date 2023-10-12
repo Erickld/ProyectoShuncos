4b9a7624e04d75d57026dc7358c593c08d5a2e66
Erickld <erickld.24@gmail.com>
2023-10-12 16:38:43 -0600

    creacion de productos

--- javascripts/carritoVentana.js
+++ javascripts/carritoVentana.js
@@ -2,12 +2,6 @@ function mostrarCarrito() {
     cart = document.querySelector('.ventana-carrito');
     cart.classList.add('mostrar');
 
-    hola = document.querySelector('.carrito-titulo');
-    console.log(hola)
-    hola.addEventListener('click', () => {
-        console.log('hola!!!')
-    })
-
 }
 
 function ocultarCarrito() {
