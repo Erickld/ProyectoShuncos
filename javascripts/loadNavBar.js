b2d02e798cd2b6d88925b5d45dac50a3d4dfb27b
Erickld <erickld.24@gmail.com>
2023-11-03 00:28:59 -0600

    navbar dinamico

--- javascripts/loadNavBar.js
+++ javascripts/loadNavBar.js
@@ -5,7 +5,7 @@ let currentUser = localStorage.getItem('currentUser');
 
 fetch(urlNavBar).then(res => res.text()).then(data => {
     containerNavBar.innerHTML = data;
-    document.getElementById('logo-shuncos-navbar').src = (fuenteNavBar == "root") ? ('./assets/img/shuncos_logo_nav_bar.png') : ('../assets/img/shuncos_logo_nav_bar.png');
+    document.getElementById('logo-shuncos-navbar').src = (fuenteNavBar == "root") ? ('./assets/img/shuncos_logo_nav_bar.webp') : ('../assets/img/shuncos_logo_nav_bar.webp');
 
     let links = document.querySelectorAll(".link-home");
     links[0].href = (fuenteNavBar == "root") ? ('./index.html') : ('../index.html');
