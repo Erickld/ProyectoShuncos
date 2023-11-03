e71d9c847cefa05b0f14a16d0e6d230ffc5bcf3d
Jesús Emmanuel <uskin825@gmail.com>
2023-11-03 04:34:15 -0600

    Añadida toda la funcionalidad de la tienda y validaciones

--- javascripts/storeProducts.js
+++ javascripts/storeProducts.js
@@ -8,7 +8,6 @@ function iniciarTienda () {
 
 function añadirBotonesTarjetas () {
     const tarjetas = document.querySelectorAll('.product-card');
-    console.log(tarjetas);
     tarjetas.forEach((tarjeta) => {
         tarjeta.addEventListener('click', crearModal);
     });
@@ -16,9 +15,11 @@ function añadirBotonesTarjetas () {
 
 function crearModal(event) {
     const tarjeta = event.currentTarget;
+    let valoresProducto = tarjeta.children.namedItem('registro-producto').value;
+    valoresProducto = JSON.parse(valoresProducto);
     const divImg = tarjeta.children.namedItem('div-imgTarjeta');
-    const img = divImg.firstElementChild; 
-    console.log(img);
+    const img = divImg.firstElementChild;
+    
 
     const modal = document.createElement('div');
     modal.setAttribute('id', 'contenedor-modal');
@@ -35,36 +36,35 @@ function crearModal(event) {
                     <div class="img-producto">
                         <img src=${img.src} alt="imagen-produto" ">
                     </div>
-                    <h3 class="nombre-producto">Guayabera</h3>
-                    <p class="descripcion">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur impedit amet nobis vel iusto similique esse commodi eaque omnis ex.</p>
+                    <h3 class="nombre-producto">${valoresProducto.modelo}</h3>
+                    <div class="descripcion-div">
+                        <ul class="caracteristicas">
+                            <li>
+                                Color: ${valoresProducto.color}
+                            </li>
+                            <li>
+                                Sexo: ${valoresProducto.sexo}
+                            </li>
+                            <li>
+                                Tipo de manga: ${valoresProducto.tipo_manga}
+                            </li>
+                        <u/>
+                    </div>
                     <form class="formularioProductoModal" action="">
                         <legend>Talla</legend>
                         <fieldset>
+                        
                             <label for="talla">Escoja una talla</label>
-                            <div class="talla-radio d-flex justify-content-around">
-                                <div>
-                                    <input type="radio" name="edad" value="adulto" id="pedido-talla-adulto">
-                                    <label for="pedido-talla-adulto">Adulto</label>
-                                </div>
-                                <div>
-                                    <input type="radio" name="edad" value="nino" id="pedido-talla-nino">
-                                    <label for="pedido-talla-nino">Niño</label>
-                                </div>
-                                
-                            </div>
-                            <div class="tallas">
+
+                            <div id="tallas" class="row">
 
                             </div>
+
                         </fieldset>
 
-                        <legend>Cantidad</legend>
-                        <fieldset class="mb-3">
-                            <label for="cantidad-producto">Cantidad</label>
-                            <input type="number" name="" id="cantidad-producto">
-                        </fieldset>
-
-                        <div class="d-block d-md-flex justify-content-md-end my-3">
-                            <button id="agregar-carrito" class="btn btn-dark" data-bs-dismiss="modal" type="button">Agregar al carrito</button>
+                        <div class="d-block d-md-flex justify-content-md-around my-3">
+                            <button id="modal-comprar" class="btn btn-dark"  type="button">Comprar</button>
+                            <button id="agregar-carrito" class="btn btn-dark"  type="button">Agregar al carrito</button>
                         </div>
                         
                     </form> <!--Formulario para el modal-->
@@ -78,8 +78,11 @@ function crearModal(event) {
     const modal_anterior = document.querySelector('#contenedor-modal');
     body.replaceChild(modal, modal_anterior);
 
-    const modal_aux = new bootstrap.Modal(document.querySelector('#modal-producto'));
-    modal_aux.show();  
+    const modal_completo = new bootstrap.Modal(document.querySelector('#modal-producto'));
+    modal_completo.show(); 
+    
+    AgregarTallas(tarjeta);
+    añadirListenersModal(modal_completo, valoresProducto);
 }
 
 
@@ -92,21 +95,22 @@ function obtenerProductos () {
 }
 
 function crearProducto (producto) {
+    const productoJSON = JSON.stringify(producto);
     const aparador = document.querySelector('#aparador');
     const contenedor = document.createElement('div');
     let clases = ['col-md-3', 'p-4'];
     contenedor.classList.add(...clases);
     const id = producto.id;
-    const salvarProducto = document.createElement('input');
-    salvarProducto.setAttribute('type', 'text');
-    salvarProducto.style.display= 'none';
-    salvarProducto.value = producto;
-    console.log(salvarProducto);
+    const registroProducto = document.createElement('input');
+    registroProducto.setAttribute('name', 'registro-producto');
+    registroProducto.setAttribute('type', 'text');
+    registroProducto.style.display= 'none';
+    registroProducto.value = productoJSON;
 
     const tarjeta = crearTarjeta(id);
     const estructuraTarjeta = crearEstructura();
     const estructuraPoblada = poblarEstructura(estructuraTarjeta, producto);
-    tarjeta.append(estructuraPoblada.divImagen, estructuraPoblada.divInfo, salvarProducto);
+    tarjeta.append(estructuraPoblada.divImagen, estructuraPoblada.divInfo, registroProducto);
     contenedor.appendChild(tarjeta);
     aparador.appendChild(contenedor);
 }
@@ -163,4 +167,164 @@ function agregarFuncionesBotones () {
         toast.show();
     }
 
+}
+
+function AgregarTallas (tarjeta) {
+    const divTallas = document.querySelector('#tallas');
+    let valoresProducto = tarjeta.children.namedItem('registro-producto').value;
+    valoresProducto = JSON.parse(valoresProducto);
+    const tallas = valoresProducto.tallas;
+    
+    tallas.forEach( talla => {
+        const divTalla = document.createElement('div');
+        let estructura = crearEstructuraTalla();
+        let estructuraPoblada = poblarEstructuraTalla(estructura, talla);
+
+        divTalla.append(estructura.radio, estructura.label);
+        divTallas.appendChild(divTalla);
+    })
+}
+
+function crearEstructuraTalla () {
+    const estructura = {
+        radio : document.createElement('input'),
+        label : document.createElement('label')
+    };
+
+    estructura.radio.id = "talla";
+    estructura.radio.type = "radio";
+    estructura.radio.name = "talla";
+
+
+ 
+    estructura.label.id = 'label-radio'; 
+    estructura.label.classList.add('label-radio');
+    estructura.label.setAttribute('for', 'talla');
+
+    return estructura;
+}
+
+function poblarEstructuraTalla (estructura, talla) {
+    estructura.radio.value = talla;
+    estructura.label.innerText = talla;
+
+    return estructura;
+}
+
+function añadirListenersModal (modal, producto) {
+    const btnComprar = document.querySelector('#modal-comprar');
+    const btnCarrito = document.querySelector('#agregar-carrito');
+
+    btnComprar.addEventListener('click', (event) => {
+        realizarCompra(modal)
+    });
+    btnCarrito.addEventListener('click', (event) => {
+        agregarAlCarrito(modal, producto)
+    });
+}
+
+function realizarCompra (modal) {
+    let talla = obtenerTalla();
+
+    if(!talla) {
+        alerta('rosa', 'por favor elija una talla');
+        return;
+    }
+
+    modal.hide();
+    window.open('./Payment.html', '_blank');
+}
+
+function agregarAlCarrito (modal, producto) {
+
+    let carrito = sessionStorage.getItem('carrito');
+    let talla = obtenerTalla();
+
+    if(!talla) {
+        alerta('rosa', 'por favor elija una talla');
+        return;
+    }
+
+    let productoCarrito = {
+        id: producto.id,
+        imagen: producto.imagen_url,
+        modelo: producto.modelo,
+        tallaElegida: talla,
+        color: producto.color,
+        precio: producto.precio
+    }
+    
+
+    if(!carrito) {
+        carrito = [];
+        carrito.push(productoCarrito);
+        let carritoJSON = JSON.stringify(carrito);
+        sessionStorage.setItem('carrito', carritoJSON);
+        modal.hide();
+        alerta('verde', 'Elemento agregado al carrito');
+    } else {
+        carrito = JSON.parse(carrito);
+        let repetido = isRepetido(productoCarrito, carrito);
+
+        if(!repetido) {
+            carrito.push(productoCarrito);
+            carrito = JSON.stringify(carrito);
+            sessionStorage.setItem('carrito', carrito);
+            modal.hide();
+            alerta('verde', 'Elemento agregado al carrito');
+        } else {
+            alerta('rosa', 'El elemento ya se encuentra en el carrito');
+        }
+        
+    }
+}
+
+
+function obtenerTalla () {
+    let tallas = document.querySelectorAll('input[name = "talla"]')
+    let tallaElegida;
+    tallas.forEach( talla => {
+        if(talla.checked) {
+            tallaElegida = talla.value;
+        }
+    })
+    return tallaElegida;
+}
+
+function isRepetido (productoCarrito, carrito) {
+    let isRepetido = false;
+
+    carrito.forEach(producto => {
+        if(productoCarrito.id === producto.id && productoCarrito.tallaElegida == producto.tallaElegida) {
+            isRepetido = true;
+        }
+    });
+
+    return isRepetido;
+}
+
+
+function alerta(color, texto) {
+    const toastLiveExample = document.getElementById('alerta-toast');
+    const alertaTxt = document.getElementById('alerta-txt');
+
+    if (color == "verde") {
+        toastLiveExample.style.backgroundColor = "#56A35B"
+        //toastLiveExample.style.boxShadow = "0 0 0 4px #56A35B"
+    }
+
+    if (color == "rosa") {
+        toastLiveExample.style.backgroundColor = "#E4007C"
+        //toastLiveExample.style.boxShadow = "0 0 0 4px #E4007C"
+    }
+
+    if (color == "azul") {
+        toastLiveExample.style.backgroundColor = "#2173b4"
+        //toastLiveExample.style.boxShadow = "0 0 0 4px #2173b4"
+    }
+
+    alertaTxt.textContent = texto;
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastBootstrap.show();
+    
 }
